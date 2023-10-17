faa810eae07484de14106168d773181f8f7a497e
Alexandr <airmanjke57@gmail.com>
2023-10-18 01:44:17 +0200

    match and stan

--- src/Formatter.php
+++ src/Formatter.php
@@ -12,7 +12,7 @@ function showFormatted(array $comparison, string $formatName)
         'stylish' => showStylish($comparison),
         'plain' => showPlain($comparison),
         'json' => showJson($comparison),
-        'foo'|'bar'=> 'wrong format',
+        default => 'wrong format',
     };
     return $result;
 }
