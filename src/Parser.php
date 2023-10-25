d47df5ff4a1dcfc4647017820c03576142ddf3b8
Alexandr <airmanjke57@gmail.com>
2023-10-25 07:09:19 +0200

    fixes

--- src/Parser.php
+++ src/Parser.php
@@ -8,10 +8,10 @@ function parse(string $pathToFile, string $extension)
 {
     switch ($extension) {
         case 'json':
-            return json_decode($fileContent, true);
+            return json_decode($pathToFile, true);
         case 'yaml':
         case 'yml':
-            return Yaml::parse($fileContent);
+            return Yaml::parse($pathToFile);
         default:
             throw new Exception('wrong extension, try json or yaml');
     }
