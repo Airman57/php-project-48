a1f38bcf055562caf529894538e7640c19b7c053
Alexandr <airmanjke57@gmail.com>
2023-11-08 07:27:20 +0100

    fixes

--- src/Differ.php
+++ src/Differ.php
@@ -14,20 +14,20 @@ function compare(array $file1, array $file2)
     $sortedKeys = sort($keys, fn($x, $y) => strcmp($x, $y));
     $result = array_map(function ($key) use ($file1, $file2) {
         if (!array_key_exists($key, $file1)) {
-            return ['key' => $key, 'condition' => 'added', 'value' => $file2[$key]];}
-
+            return ['key' => $key, 'condition' => 'added', 'value' => $file2[$key]];
+        }
         if (!array_key_exists($key, $file2)) {
-            return ['key' => $key, 'condition' => 'removed', 'value' => $file1[$key]];}
-
+            return ['key' => $key, 'condition' => 'removed', 'value' => $file1[$key]];
+        }
         if (is_array($file1[$key]) && is_array($file2[$key])) {
-            return ['key' => $key, 'condition' => 'array', 'children' => compare($file1[$key], $file2[$key])];}
-
+            return ['key' => $key, 'condition' => 'array', 'children' => compare($file1[$key], $file2[$key])];
+        }
         if ($file1[$key] !== $file2[$key]) {
-            return ['key' => $key, 'condition' => 'changed','oldValue' => $file1[$key], 'newValue' => $file2[$key]];}
-
+            return ['key' => $key, 'condition' => 'changed','oldValue' => $file1[$key], 'newValue' => $file2[$key]];
+        }
         if ($file1[$key] == $file2[$key]) {
-            return ['key' => $key, 'condition' => 'unchanged', 'value' => $file1[$key]];}
-
+            return ['key' => $key, 'condition' => 'unchanged', 'value' => $file1[$key]];
+        }
     }, $sortedKeys);
     return $result;
 }
